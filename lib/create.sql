885cf7f5609961ee87e70560b64d81265a94cad0
Gad-Ongoro <gadongoro1@gmail.com>
2023-12-19 10:05:16 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER,
+  name VARCHAR(50),
+  age INTEGER(50),
+  sex VARCHAR(50),
+  color VARCHAR(50),
+  temperament VARCHAR(50),
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+(1, "Mr. Chocolate", 25, "M", "White", "Melancholic", "true"),
+(2, "Rowdy", 26, "F", "Black", "Phlegmatic", "true"),
+(3, "Tabitha", 27, "F", "White", "Choleric", "true")
+(4, "Sergeant Brown", 28, "M", "Black", "Phlegmatic", "true")
+(5, "Melissa", 25, "F", "White", "Choleric", "true")
+(6, "Grinch", 29, "M", "Black", "Melancholic", "true")
+(7, "Wendy", 28, "F", "White", "Sanguine", "true")
+(8, NULL, NULL, NULL, NULL, NULL, NULL);
